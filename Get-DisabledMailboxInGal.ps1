9dfa480ab715929b58f4e82e83c76ce4a6cc99d8
raulerivassanz <stevehunter946@gmail.com>
2018-05-02 15:12:51 -0400

    Added function Get-DisabledMailboxInGal

---
+++ Get-DisabledMailboxInGal.ps1
@@ -0,0 +1,57 @@
+Function Get-DisabledMailboxInGal
+{
+    <#
+    .SYNOPSIS
+    Returns a mailbox if the following conditions are true:
+    - the Active Directory account is disabled
+    - the mailbox is present in the GAL
+
+    .DESCRIPTION
+    Returns a mailbox if the following conditions are true:
+    - the Active Directory account is disabled
+    - the mailbox is present in the GAL
+
+    .EXAMPLE
+    Get-DisabledMailboxInGal
+
+    .INPUTS
+    None.
+
+    .OUTPUTS
+    None.
+
+    .NOTES
+    Raymond Jette
+
+    #>
+    [OutputType([System.Management.Automation.PSCustomObject])]
+    [CmdletBinding()]
+    param ()
+
+    $FilterBlock = {
+        (HiddenFromAddressListsEnabled -eq $false) -and
+        (ExchangeUserAccountControl -eq 'AccountDisabled') -and
+        (ResourceType -ne 'Room')
+    }
+
+    $GetMailboxParams = @{
+        ResultSize    = 'Unlimited'
+        ErrorAction   = 'Stop'
+        Filter        = $FilterBlock
+    }
+
+    try {
+
+        if (Get-Command Get-Mailbox -ErrorAction 'SilentlyContinue') {
+
+            foreach ($item in (Get-Mailbox @GetMailboxParams)) {
+
+                [PSCustomObject]@{
+                    Name = $item.Name
+                    Alias = $item.Alias
+                }
+            }
+        } else { Write-Error -Message "The Get-Mailbox cmdlet is not available.  Make sure Exchange cmdlets are available." }
+
+    } catch { $PSItem.Exception.Message }
+}
