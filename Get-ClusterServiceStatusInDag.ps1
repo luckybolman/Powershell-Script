9ceefe8eabd54ec02e03464d677cf80ce7dc7c4e
Raymond Jette <rjette@mestek.com>
2018-05-05 15:42:34 -0400

    Added function to determin the service status on all members of all  DAG's

---
+++ Get-ClusterServiceStatusInDag.ps1
@@ -0,0 +1,43 @@
+Function Get-ClusterServiceStatusInDag
+{
+    <#
+    .SYNOPSIS
+    Gets all mailbox database servers in the database availability group and returns an object contaning the name and status of the 
+    cluster service.
+
+    .DESCRIPTION
+    Gets all mailbox database servers in the database availability group and returns an object contaning the name and status of the 
+    cluster service.
+
+    .EXAMPLE
+    Get-ClusterServiceStatusInDag
+
+    .INPUTS
+    None.  Get-ClusterServiceStatusInDag does not have any parameters.
+
+    .OUTPUTS
+    System.Management.Automation.PSCustomObject
+
+    .NOTES
+    Raymond Jette
+
+    #>
+    [OutputType([System.Management.Automation.PSCustomObject])]
+    [CmdletBinding()]
+    param ()
+
+    foreach ($dag in (Get-DatabaseAvailabilityGroup)) {
+        
+        foreach ($server in ((Get-DatabaseAvailabilityGroup).Servers)) {
+
+            $GetWmiObjectParams = @{class = 'win32_service'; ComputerName = $server}
+            $query = Get-WmiObject @GetWmiObjectParams | Where-Object { $PSItem.Name -eq 'ClusSvc' } 
+
+            [PSCustomObject]@{
+                Server = $server
+                MemberOf = $dag
+                Service = $query.State 
+            }
+        }
+    }
+} # Get-ClusterServiceStatusInDag
