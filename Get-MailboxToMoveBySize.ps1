0a1d2e7f15099d97b0eac07e5c35ad4352539344
Raymond Jette <rjette@mestek.com>
2018-05-05 14:41:25 -0400

    Added function to help recommend which mailboxes should be moved.

---
+++ Get-MailboxToMoveBySize.ps1
@@ -0,0 +1,56 @@
+Function Get-MailboxToMoveBySize 
+{
+    <#
+    .SYNOPSIS
+    Returns a collection of mailboxes that should be moved to reach the desired size.
+
+    .DESCRIPTION
+    Returns a collection of mailboxes that should be moved to reach the size give by Size in MB.
+    
+    .EXAMPLE
+    Get-MailboxToMoveBySize -Database db00 -Size 100
+
+    .EXAMPLE
+    Get the mailboxes to move.  Exclude any mailboxes that are currenlty being moved.  Then move the
+    mailboxes to the given target database
+
+    (Get-MailboxToMoveBySize -Database db00 -Size 112000 -Filter ((Get-MoveRequest).DisplayName)).DisplayName | New-MoveRequest -BadItemLimit 50 -TargetDatabase db01
+    
+    .NOTES
+    Raymond Jette
+
+    #>
+    [CmdletBinding()]
+    param (
+        # Database
+        [parameter(
+            mandatory=$true,
+            HelpMessage='The database you will be moving from'
+        )]
+        [string]$Database,
+
+        # Size
+        [parameter(
+            mandatory=$true,
+            HelpMessage='The size, in MB, that you would like to move from database'
+        )]
+        [int64]$Size,
+
+        # Filter
+        [String[]]$Filter
+    )
+
+    $mailboxStats = Get-MailboxStatistics -Database $Database -ErrorAction 'SilentlyContinue' | 
+        Select-Object DisplayName, @{Name="SizeMB";Expression={[math]::round( [int64]$_.TotalItemSize.ToString().TrimEnd(' bytes)').Split('(')[1] / 1MB)}} |
+        Sort-Object -Property SizeMB -descending
+
+    $mailboxStats | ForEach-Object { 
+
+        if (($_.SizeMB -gt 0) -and  ($_.SizeMB -le $Size) ) {
+            If (  ($Filter -NotContains $_.DisplayName ) ) { 
+                $_
+                $Size -= $_.SizeMB
+            }
+        }
+    }  
+} # Get-MailboxToMoveBySize
