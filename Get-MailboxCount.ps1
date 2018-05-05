6094d5ce2c8458536ebeb8b3a431594f68dff1d2
raulerivassanz <stevehunter946@gmail.com>
2018-05-05 16:08:52 -0400

    Added function to return mailbox count per database

---
+++ Get-MailboxCount.ps1
@@ -0,0 +1,62 @@
+Function Get-MailboxCount
+{
+    <#
+    .SYNOPSIS
+    Returns the number of mailboxes that exist in a given mailbox database.
+    
+    .DESCRIPTION
+    Returns the number of mailboxes that exist in a given mailbox database.
+    
+    .PARAMETER DatabaseName
+    The name of one of more mailbox databases.
+
+    .EXAMPLE
+    Get-MailboxCount -Database 'db01', 'db02'
+
+    .EXAMPLE
+    Get-MailboxCount -Database (Get-MailboxDatabase)
+
+    DatabaseName   MailboxesOnDb
+    ------------   -------------
+    db0            20
+    db1            32
+    db2            48
+    db3            27
+    db4            18
+    
+    .NOTES
+    Raymond Jette
+
+    #>
+    [CmdletBinding()]
+    Param (
+        [Parameter(
+            Mandatory,
+            ValueFromPipeline,
+            ValueFromPipelineByPropertyName
+        )]
+        [ValidateScript({$_ | Get-MailboxDatabase})]
+        [Alias('Name')]
+        [String[]]$DatabaseName
+    )
+
+    PROCESS {
+
+        Try {
+
+            Write-Verbose -Message 'Looping though databases...'
+            foreach ($db in $DatabaseName) {
+
+                Write-Verbose "Current database: $db"
+                If ($query = (Get-Mailbox -Database $db)) {
+
+                    [PSCustomObject]@{
+                        DatabaseName  = $db
+                        MbxCount = $query.count
+                    }
+                }
+            }
+        }
+        Catch {$_.Exception.Message}
+    }
+} # Get-MailboxCount
