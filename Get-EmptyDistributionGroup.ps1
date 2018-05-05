e366a6fad79648f5e91ae57d37bc84e2d27d5f62
Raymond Jette <rjette@mestek.com>
2018-05-05 09:56:51 -0400

    Added function Get-EmptyDistributionGroup

---
+++ Get-EmptyDistributionGroup.ps1
@@ -0,0 +1,40 @@
+Function Get-EmptyDistributionGroup
+{
+    <#
+    .SYNOPSIS
+    Returns empty distribution groups from Exchange.
+
+    .DESCRIPTION
+    Returns emtpy distribution groups from Exchange.  An empty distribution gorup is one which has 0 members.
+
+    .EXAMPLE
+    Get-EmptyDistributionGroup
+
+    .INPUTS 
+    None.
+
+    .OUTPUTS
+    System.Management.Automation.PSCustomObject
+
+    .NOTES
+    Raymond Jette
+
+    #>
+    [OutputType([System.Management.Automation.PSCustomObject])]
+    [CmdletBinding()]
+    param ()
+    
+    $distGroupParams = @{ResultSize = 'Unlimited'; ErrorAction = 'Stop'}
+    foreach ($distGroup in (Get-DistributionGroup @distGroupParams)) {
+
+        If (-Not (Get-DistributionGroupMember -Identity $distGroup.Name)) {
+
+            [PSCustomObject]@{
+                DisplayName = $distGroup.DisplayName
+                Identity = $distGroup.Identity
+                GroupType = $distGroup.GroupType
+                PrimarySmtp = $distGroup.PrimarySmtpAddress
+            }
+        }
+    }
+} # Get-EmptyDistributionGroup
