c9fb43aa4fc7a363aa1ccea8d2c74e7f445b95ea
raulerivassanz <stevehunter946@gmail.com>
2018-05-05 20:16:28 -0400

    Added function to locate orphaned SIDs in Exchange mailboxes.

---
+++ Get-MailboxOrphanedSID.ps1
@@ -0,0 +1,46 @@
+Function Get-MailboxOrphanedSID
+{
+    <#
+    .SYNOPSIS
+    Can be used to locate mailboxes with orphaned SIDs.
+
+    .DESCRIPTION
+    Can be used to locate mailboxes with orphaned SIDs.
+
+    .EXAMPLE
+    Get-MailboxOrphanedSID
+
+    .INPUTS
+    None.  Get-MailboxOrphanedSID does not have any parameters.
+
+    .OUTPUTS
+    System.Management.Automation.PSCustomObject
+
+    .NOTES
+    Raymond Jette
+
+    #>
+    [OutputType([PSCustomObject])]
+    [CmdletBinding()]
+    param ()
+
+    foreach ($mailbox in (Get-Mailbox -ResultSize Unlimited)) {
+    
+        $sid = @()
+        foreach ($permission in (Get-MailboxPermission -identity $mailbox.SamAccountName)) {
+
+            if (($permission.user.tostring() -like "S-*-*") -and ($permission.IsInherited -eq $false)) {
+
+                $sid += $permission.user.tostring()
+            }
+        }
+
+        if ($sid) {
+
+            [PSCustomObject]@{
+                Mailbox = ($Mailbox.Identity -split '/')[-1]
+                SIDs = $sid
+            }
+        }
+    }
+} # Get-MailboxOrphanedSID
